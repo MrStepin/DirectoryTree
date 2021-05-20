b7655b5174ccd247cced5db1cf76c24f4b995b70
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-05-20 22:26:35 +0300

    .

--- DirectoryTree/Program.cs
+++ DirectoryTree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,29 @@ namespace DirectoryTree
 {
     class Program
     {
-        static void Main(string[] args)
+        static string DirectoriesTree(string path)
         {
+            string[] dirs = Directory.GetDirectories(path);
+
+            if (dirs.Length != 0)
+            {
+                foreach (string dir in dirs)
+                {
+                    DirectoriesTree(dir);
+                    Console.WriteLine(dir);
+                }
+            }
+            return path;
         }
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path:");
+            string path = Console.ReadLine();
+
+            Console.WriteLine(DirectoriesTree(path));
+            Console.ReadKey();
+        }
+
     }
 }
